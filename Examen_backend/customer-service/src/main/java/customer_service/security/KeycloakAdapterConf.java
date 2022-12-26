d204d1a9f32fcca09ee745d3a526738f2f445f7e
ELYOUSFI Mohamed <73071155+ElyousfiMohamed@users.noreply.github.com>
2022-12-26 12:23:19 +0100

    Security with keycload

---
+++ Examen_backend/customer-service/src/main/java/customer_service/security/KeycloakAdapterConf.java
@@ -0,0 +1,13 @@
+package customer_service.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakAdapterConf {
+    @Bean
+    KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
